9e01d8b08a0ee63a60fe1adf9a0450b9b37e480c
Stefan <Stefan@DESKTOP-P1OADI2>
2024-11-05 10:04:55 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1._1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"The minimum value of an int variable is {int.MinValue} and the maximum value of an in is {int.MaxValue} ");
+            Console.WriteLine();
+
+            Console.WriteLine($"The minimum value fo a float value is {float.MinValue} and the maximum value is {float.MaxValue}");
+            Console.WriteLine();
+
+            Console.WriteLine($"The minimum value of a decimal value is {decimal.MinValue} and the maximum value is {decimal.MaxValue}");
+            Console.WriteLine();
+
+            Console.WriteLine($"The minimum value of a double value is {double.MinValue} and the maximum value is {double.MaxValue}");
+
+            Console.Read(); 
+
+        }
+    }
+}
